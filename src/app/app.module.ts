71140c0deaaea1f426f7be0d737d3c7c70da7141
baryo <baryo.stuff@gmail.com>
2017-07-19 00:19:41 +0300

    creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ImagesListComponent } from './images-list/images-list.component';
+import { ImageComponent } from './image/image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ImagesListComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/images-list/images-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-images-list',
+  templateUrl: './images-list.component.html',
+  styleUrls: ['./images-list.component.css']
+})
+export class ImagesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
